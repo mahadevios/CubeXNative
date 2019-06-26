fb7529cea428114d3349f69bffa505b05ff1c7df
Mahadev Mandale <mahadevmandale@yahoo.com>
2019-06-26 15:00:12 +0530

    * CubeXNative.iOS.csproj: commit
    * packages.config:
    * CubeXNative.Droid.csproj:
    * SampleViewModel.cs:
    * SampleViewController.cs:

--- CubeXNative/ViewModels/SampleViewModel.cs
+++ CubeXNative/ViewModels/SampleViewModel.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Autofac;
 using CubeXNative.Services;
+using Acr.UserDialogs;
 
 namespace CubeXNative
 { 
@@ -25,7 +26,11 @@ namespace CubeXNative
 
             IsBusy = true;
 
+            UserDialogs.Instance.ShowLoading("Loading..", MaskType.Black);
+
             var _items = await getVertcalList();
+
+            UserDialogs.Instance.HideLoading();
 
             verticalItemLists.ReplaceRange(_items.itemVerticals);
 
